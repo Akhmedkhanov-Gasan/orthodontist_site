7bf23a66722a09f25e3619eebc73110f3f394185
Akhmedkhanov Gasan <ldagonl@gmail.com>
2024-09-10 13:54:15 +0300

    New version 0.5
    - update execute_from_command_line

--- manage.py
+++ manage.py
@@ -20,5 +20,16 @@ def main():
 
 
 if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orthodontist_site.settings')  # Убедись, что 'orthodontist_site' — это название твоего проекта
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
     port = os.environ.get('PORT', '8000')
     execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:' + port])
