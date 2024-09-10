501557c1968abfd7dd5bee6ffe55632086d45378
Akhmedkhanov Gasan <ldagonl@gmail.com>
2024-09-10 18:16:23 +0300

    New version 0.6
    - add base.html
    - add active_page
    - edit manage.py

--- main/views.py
+++ main/views.py
@@ -2,32 +2,25 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')  # Соответствует пути '' в urls.py
-
+    return render(request, 'index.html', {'active_page': 'home'})
 
 def about(request):
-    return render(request, 'about.html')  # Соответствует пути 'about/' в urls.py
-
+    return render(request, 'about.html', {'active_page': 'about'})
 
 def appointment(request):
-    return render(request, 'appointment.html')  # Соответствует пути 'appointment/' в urls.py
-
+    return render(request, 'appointment.html', {'active_page': 'appointment'})
 
 def contact(request):
-    return render(request, 'contact.html')  # Соответствует пути 'contact/' в urls.py
-
+    return render(request, 'contact.html', {'active_page': 'contact'})
 
 def price(request):
-    return render(request, 'price.html')  # Соответствует пути 'price/' в urls.py
-
+    return render(request, 'price.html', {'active_page': 'price'})
 
 def service(request):
-    return render(request, 'service.html')  # Соответствует пути 'service/' в urls.py
-
+    return render(request, 'service.html', {'active_page': 'service'})
 
 def team(request):
-    return render(request, 'team.html')  # Соответствует пути 'team/' в urls.py
-
+    return render(request, 'team.html', {'active_page': 'team'})
 
 def testimonial(request):
-    return render(request, 'testimonial.html')  # Соответствует пути 'testimonial/' в urls.py
+    return render(request, 'testimonial.html', {'active_page': 'testimonial'})
--- manage.py
+++ manage.py
@@ -2,8 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management import execute_from_command_line  # Добавь этот импорт
-
+from django.core.management import execute_from_command_line
 
 def main():
     """Run administrative tasks."""
@@ -18,7 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orthodontist_site.settings')  # Убедись, что 'orthodontist_site' — это название твоего проекта
 
@@ -32,4 +30,9 @@ if __name__ == '__main__':
         ) from exc
 
     port = os.environ.get('PORT', '8000')
-    execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:' + port])
+    if os.environ.get('DJANGO_DEVELOPMENT', 'true').lower() in ['true', '1']:
+        # Использовать 127.0.0.1 для локальной разработки
+        execute_from_command_line([sys.argv[0], 'runserver', '127.0.0.1:' + port])
+    else:
+        # Использовать 0.0.0.0 для развертывания
+        execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:' + port])
